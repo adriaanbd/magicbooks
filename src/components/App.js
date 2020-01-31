573ce04016ef187329cf777a480c5b31d2518817
adriaanbd <adriaanbd@gmail.com>
2020-01-31 15:13:46 -0500

    moving app to components

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import BooksList from './containers/BooksList';
-import BooksForm from './components/BooksForm';
+import BooksList from '../containers/BooksList';
+import BooksForm from './BooksForm';
 
 function App() {
   return (
