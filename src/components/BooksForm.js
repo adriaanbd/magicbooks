a3ee8aa8e122f854c0da880240b1233b3c496699
adriaanbd <adriaanbd@gmail.com>
2020-01-31 21:29:28 -0500

    implemented handle change

--- src/components/BooksForm.js
+++ src/components/BooksForm.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const CATEGORIES = [
   'Action',
@@ -10,28 +10,43 @@ const CATEGORIES = [
   'Sci-Fi',
 ];
 
-function createCategories() {
-  return (
-    <div>
-      <label htmlFor="categories" />
-          Category:
-      <select id="categories">
-        { CATEGORIES.map((cat, idx) => <option key={idx} value={cat}>{cat}</option>) }
-      </select>
-    </div>
-  );
-}
-
 function BooksForm() {
+  const [bookData, setBookData] = useState({ title: '', category: '' });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setBookData(prevBookData => ({
+      ...prevBookData,
+      [name]: value,
+    }));
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault(); // prevents browser reload
+  }
+
   return (
-    <form>
-      <label htmlFor="title">
-        Title:
-        <input type="text" name="title" id="title" />
-      </label>
-      { createCategories() }
-      <input type="submit" value="Submit" />
-    </form>
+    <>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="title">
+          Title:
+          <input
+            type="text"
+            name="title"
+            id="title"
+            value={bookData.title}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="categories">
+          Category:
+          <select id="categories" name="category" value={bookData.category} onChange={handleChange}>
+            { CATEGORIES.map((cat, idx) => <option key={idx} value={cat}>{cat}</option>) }
+          </select>
+        </label>
+        <button type="submit">Add Book</button>
+      </form>
+    </>
   );
 }
 
