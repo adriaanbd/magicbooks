ea877effb5081f11e788fd7691d3540ac0c17a3c
adriaanbd <adriaanbd@gmail.com>
2020-02-15 14:54:15 -0500

    design mvp

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
 
 function Nav() {
   return (
     <nav>
       <div id="nav-left">
-        <h1>MagicBooks CMS</h1>
+        <h1 id="company-name">MagicBooks CMS</h1>
         <div className="navLink">Books</div>
         <div className="navLink">Categories</div>
       </div>
-      <div id="nav-right">Avatar</div>
+      <div id="nav-right">
+        <FontAwesomeIcon icon={faUserCircle} size="2x" id="avatar" />
+      </div>
     </nav>
   );
 }
--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-// import Book from '../components/Book';
-// import { removeBook, changeFilter } from '../actions/index';
-// import CategoryFilter from '../components/CategoryFilter';
-import { removeBook } from '../actions/index';
+import { removeBook, changeFilter } from '../actions/index';
+import CategoryFilter from '../components/CategoryFilter';
 import BookCard from '../components/BookCard';
 
 function BooksList() {
@@ -14,9 +12,9 @@ function BooksList() {
     dispatch(removeBook(book));
   }
 
-  // function handleChangeFilter(category) {
-  //   dispatch(changeFilter(category));
-  // }
+  function handleChangeFilter(category) {
+    dispatch(changeFilter(category));
+  }
 
   function filteredBooks(category) {
     if (category === 'All') {
@@ -27,6 +25,10 @@ function BooksList() {
 
   return (
     <>
+      <CategoryFilter
+        handleChangeFilter={handleChangeFilter}
+        filter={filter}
+      />
       { filteredBooks(filter).map(book => (
         <BookCard
           key={book.bookId}
